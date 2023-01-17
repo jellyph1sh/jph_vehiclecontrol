cf71f609dd409c4f4ddc5073f6d43e9e46b72122
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-01-17 10:41:51 +0100

    :rocket: | Deploy base of the script.

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+description "Script to control a vehicle."
+author "JellyPh1sh"
+
+client_script "cl_vehiclecontrol.lua"
+
+server_script "sv_vehiclecontrol.lua"
+
+shared_script "config.lua"
